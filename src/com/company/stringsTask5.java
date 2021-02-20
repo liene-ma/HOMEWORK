5fdce1c9f141a3813b19f4e2d4e5a7a9be61334a
liene-ma <liene.malkalne@gmail.com>
2021-02-20 21:04:58 +0000

    strings assignment 5

---
+++ src/com/company/stringsTask5.java
@@ -0,0 +1,35 @@
+package com.company;
+
+public class stringsTask5 {
+//    static String test = "Mom";
+    static String test = "Was it a car or a cat I saw?";
+//    static String test = "Madam, in Eden, I'm Adam.";
+//    static String test = "Yo, banana boy!";
+//    static String test = "El mundo entero";
+
+
+
+    public static void main (String[] args) {
+
+        System.out.println(test); //print out the sentence
+
+        String letters = justLetters(); //the new string with just letters
+        System.out.println(letters);
+
+        StringBuffer lettersRev = new StringBuffer(letters); //reverse the letters using StringBuffer
+        lettersRev.reverse();
+        System.out.println(lettersRev);
+
+        String reverse = lettersRev.toString(); //StringBuffer to string
+
+        System.out.println("It is a palindrome: " + letters.equals(reverse)); //check if it is a palindrome
+
+    }
+
+    public static String justLetters() { //method to convert string sentence to just letters
+        test = test.toLowerCase();
+        String letters = test.replaceAll("[^a-z]", "");
+        return letters;
+    }
+
+}
